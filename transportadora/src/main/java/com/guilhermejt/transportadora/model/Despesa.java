4beac1178992b7a6435cabc7723bc8eaad2d309c
GuilhermeJT <guilhermej.2309@gmail.com>
2026-03-04 16:34:52 -0400

    Parcialmente concluido beck end de despesas gerais

---
+++ transportadora/src/main/java/com/guilhermejt/transportadora/model/Despesa.java
@@ -0,0 +1,47 @@
+package com.guilhermejt.transportadora.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "despesa_geral")
+public class Despesa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "data")
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "veiculo")
+    private Veiculo veiculo;
+
+    @ManyToOne
+    @JoinColumn(name = "motorista")
+    private Usuario motorista;
+
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa")
+    private Empresa empresa;
+
+    private Integer nf;
+
+    private BigDecimal valor;
+
+
+
+}
